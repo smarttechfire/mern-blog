ce646db4c85e5286d55e95f103ab9679e0aa4583
smarttechfire <educationfire552@gmail.com>
2024-05-12 19:53:23 +0530

    create a signup API route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json())
 
 mongoose
   .connect(process.env.MONGODB)
@@ -21,4 +24,5 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
 
