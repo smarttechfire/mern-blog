7efff04ad4e6f37bfc77bb8a304cbfa8ffdb3ff6
smarttechfire <educationfire552@gmail.com>
2024-05-18 20:19:21 +0530

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,18 +1,102 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  
+  ref,
+  
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../FirebaseConfig";
+
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  console.log(imageFileUploadProgress,imageFileUploadError);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  // console.log(imageFile,imageFileUrl);
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    const storage = getStorage(app);
+    const fileName = imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "could not upload image (File must be less than 2MB)"
+        );
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
+ 
+
   return (
     <div className=" max-w-lg mx-auto p-3 w-full">
       <h1 className=" my-7 text-center font-semibold text-3xl">Profile</h1>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
+        ref={filePickerRef}
+        hidden
+      />
       <form className=" flex flex-col gap-4">
-        <div className=" w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-          <img src={currentUser.profilePicture} alt="user" className=" rounded-full w-full h-full object-cover border-8 border-[lightgray]" />
+        <div
+          className=" w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt="user"
+            className=" rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
         </div>
-        <TextInput type="text" id="username" placeholder="Username" defaultValue={currentUser.username} />
-        <TextInput type="email" id="email" placeholder="Email" defaultValue={currentUser.email} />
+        {imageFileUploadError && <Alert color='failure' >{imageFileUploadError}</Alert>}
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="Username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="Email"
+          defaultValue={currentUser.email}
+        />
         <TextInput type="password" id="password" placeholder="password" />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>Update</Button>
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
       </form>
       <div className=" text-red-500 flex justify-between mt-5">
         <span className=" cursor-pointer">Delete Account</span>
