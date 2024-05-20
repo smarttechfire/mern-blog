0a33276580b3b173df425ca271a38bf9130be428
smarttechfire <educationfire552@gmail.com>
2024-05-20 07:48:30 +0530

    add update user API route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGODB)
@@ -23,17 +25,16 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 // app.use('/api/auth',authRoutes);
 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message
-  })
-})
-
+    message,
+  });
+});
