2abf3889e671770b454617d5ba03ecd274f326f9
smarttechfire <educationfire552@gmail.com>
2024-05-22 18:56:05 +0530

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { create } from '../controllers/post.controller.js';
-import {verifyToken} from "../utils/verifyUser.js"
+import express from "express";
+import { create, getPosts } from "../controllers/post.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
-router.post('/create',verifyToken,create);
-
+router.post("/create", verifyToken, create);
+router.get("/getposts", getPosts);
 
 export default router;
