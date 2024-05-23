05a0765274bc0a4ed103a20ee60abe9a4ca2e9fa
smarttechfire <educationfire552@gmail.com>
2024-05-23 21:05:28 +0530

    create a delete post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { create, getPosts } from "../controllers/post.controller.js";
+import { create, deletepost, getPosts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userid",verifyToken,deletepost);
 
 export default router;
