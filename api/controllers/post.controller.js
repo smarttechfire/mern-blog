2dcf4214e0edf6bb666be14fb86a1d5e8d0d7c08
smarttechfire <educationfire552@gmail.com>
2024-05-26 09:17:40 +0530

    fix bugs in getposts & create update post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,4 @@
-import { query } from "express";
+import { json, query } from "express";
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -29,18 +29,21 @@ export const getPosts = async (req, res, next) => {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === 'asc' ? 1 : -1;
-    const posts =await Post.find({
-      ...(req.query.userId && {userId: req.query.userId}),
-      ...(req.query.category && {userId: req.query.category}),
-      ...(req.query.slug && {userId: req.query.slug}),
-      ...(req.query.postId && {userId: req.query.postId}),
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
-          {title: {$regex: req.query.searchTerm, $options: 'i'}},
-          {content: {$regex: req.query.searchTerm,$options: 'i'}}
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
         ],
       }),
-    }).sort({ updatedAt: sortDirection}).skip(startIndex).limit(limit);
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
     const totalPosts = await Post.countDocuments();
 
     const now = new Date();
@@ -50,13 +53,13 @@ export const getPosts = async (req, res, next) => {
       now.getDate()
     );
     const lastMonthPosts = await Post.countDocuments({
-      createdAt: {$gte: oneMonthAgo},
+      createdAt: { $gte: oneMonthAgo },
     });
     res.status(200).json({
       posts,
       totalPosts,
       lastMonthPosts,
-    })
+    });
   } catch (error) {
     next(error);
   }
@@ -64,11 +67,34 @@ export const getPosts = async (req, res, next) => {
 
 export const deletepost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delete this post'));
+    return next(errorHandler(403, "You are not allowed to delete this post"));
   }
   try {
     await Post.findByIdAndDelete(req.params.postId);
-    res.status(200).json('The post has been deleted');
+    res.status(200).json("The post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update thish post"));
+  }
+  try {
+    const updatePost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200) / json(updatePost);
   } catch (error) {
     next(error);
   }
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { create, deletepost, getPosts } from "../controllers/post.controller.js";
+import { create, deletepost, getPosts, updatepost } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
 
 
 export default router;
